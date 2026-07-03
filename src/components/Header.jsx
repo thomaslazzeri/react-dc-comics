93bc1c348c565247bef482b611643bb6d8fcab0f
thomas <thomaslazzeri3@gmail.com>
2026-07-03 19:49:00 +0200

    fix: add alt text to logo image and implement Nav component in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,30 @@
+import './Header.css';
+
 const Logo = () => (
-    <img src="img/dc-logo-bg.png" alt="" />
+    <img src="img/dc-logo-bg.png" alt="Logo" />
+)
+const Nav = () => (
+    <nav>
+        <ul>
+            <li>
+                <a href="#">CHARACTERS</a>
+                <a href="#" className='active'>COMICS</a>
+                <a href="#">MOVIES</a>
+                <a href="#">TV</a>
+                <a href="#">GAMES</a>
+                <a href="#">COLLECTIBLES</a>
+                <a href="#">VIDEOS</a>
+                <a href="#">FANS</a>
+                <a href="#">NEWS</a>
+                <a href="#">SHOP</a>
+            </li>
+        </ul>
+    </nav>
 )
 
 export const Header = () => (
 <header>
     <Logo />
-    <nav></nav>
+    <Nav />
 </header>
 );
