7e5104ef90fd693a30853cfae3d4033d87af8681
thomas <thomaslazzeri3@gmail.com>
2026-07-04 20:45:56 +0200

    style: enhance layout and styling in Header and index files; update font links and structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,21 +6,19 @@ const Logo = () => (
 const Nav = () => (
     <nav>
         <ul>
-            <li>
-                <li><a href="#">CHARACTERS</a></li>
-                <li><a href="#" className='active'>COMICS</a></li>
-                <li><a href="#">MOVIES</a></li>
-                <li><a href="#">TV</a></li>
-                <li><a href="#">GAMES</a></li>
-                <li><a href="#">COLLECTIBLES</a></li>
-                <li><a href="#">VIDEOS</a></li>
-                <li><a href="#">FANS</a></li>
-                <li><a href="#">NEWS</a></li>
-               <li><a href="#">SHOP</a></li> 
-            </li>
+            <li><a href="#">CHARACTERS</a></li>
+            <li><a href="#" className='active'>COMICS</a></li>
+            <li><a href="#">MOVIES</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">GAMES</a></li>
+            <li><a href="#">COLLECTIBLES</a></li>
+            <li><a href="#">VIDEOS</a></li>
+            <li><a href="#">FANS</a></li>
+            <li><a href="#">NEWS</a></li>
+            <li><a href="#">SHOP</a></li>
         </ul>
     </nav>
-)
+);
 
 export const Header = () => (
     <header>
