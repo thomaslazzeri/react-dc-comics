19033ec27dc54aed3b14f939a901a9ed3c2be8f3
thomas <thomaslazzeri3@gmail.com>
2026-07-11 21:54:48 +0200

    feat: update Header component to accept dynamic navLinks as props

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,22 @@ import { Main } from './components/Main';
 import { BlueBar } from './components/BlueBar';
 import comics from './comics';
 
+const navLinks = [
+    { id: 1, text: "CHARACTERS", isActive: false },
+    { id: 2, text: "COMICS", isActive: true },
+    { id: 3, text: "MOVIES", isActive: false },
+    { id: 4, text: "TV", isActive: false },
+    { id: 5, text: "GAMES", isActive: false },
+    { id: 6, text: "COLLECTIBLES", isActive: false },
+    { id: 7, text: "VIDEOS", isActive: false },
+    { id: 8, text: "FANS", isActive: false },
+    { id: 9, text: "NEWS", isActive: false },
+    { id: 10, text: "SHOP", isActive: false },
+];
+
 const App = () => (
   <>
-    <Header />
+    <Header links={navLinks}/>
     <Main listaFumetti={comics}/>
     <BlueBar />
     <Footer />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,61 +1,15 @@
-/* import './Header.css';
-
-
-const Logo = () => (
-    <img src="img/dc-logo.png" alt="Logo" />
-);
-
-const Nav = () => (
-    <nav>
-        <ul>
-            <li><a href="#">CHARACTERS</a></li>
-            <li><a href="#" className='active'>COMICS</a></li>
-            <li><a href="#">MOVIES</a></li>
-            <li><a href="#">TV</a></li>
-            <li><a href="#">GAMES</a></li>
-            <li><a href="#">COLLECTIBLES</a></li>
-            <li><a href="#">VIDEOS</a></li>
-            <li><a href="#">FANS</a></li>
-            <li><a href="#">NEWS</a></li>
-            <li><a href="#">SHOP</a></li>
-        </ul>
-    </nav>
-);
-
-export const Header = () => (
-    <header>
-        <Logo />
-        <Nav />
-    </header>
-);
- */
-
 import './Header.css';
 
-const navLinks = [
-    { id: 1, text: "CHARACTERS", isActive: false },
-    { id: 2, text: "COMICS", isActive: true },
-    { id: 3, text: "MOVIES", isActive: false },
-    { id: 4, text: "TV", isActive: false },
-    { id: 5, text: "GAMES", isActive: false },
-    { id: 6, text: "COLLECTIBLES", isActive: false },
-    { id: 7, text: "VIDEOS", isActive: false },
-    { id: 8, text: "FANS", isActive: false },
-    { id: 9, text: "NEWS", isActive: false },
-    { id: 10, text: "SHOP", isActive: false },
-];
-
 const Logo = () => (
     <img src="img/dc-logo.png" alt="Logo" />
 );
 
-const Nav = () => (
+const Nav = (props) => (
     <nav>
         <ul>
-            {navLinks.map((link) => (
+            {props.links.map((link) => (
                 <li key={link.id}>
-                       <a href="#" 
-                        className={link.isActive ? 'active' : ''}>
+                    <a href="#" className={link.isActive ? 'active' : ''}>
                         {link.text}
                     </a>
                 </li>
@@ -64,9 +18,9 @@ const Nav = () => (
     </nav>
 );
 
-export const Header = () => (
-    <header>
-        <Logo />
-        <Nav />
+export const Header = (props) => (
+    <header className="header">
+            <Logo />
+            <Nav links={props.links} />
     </header>
 );
